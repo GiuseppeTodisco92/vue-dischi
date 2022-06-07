64327afcae5a1dad7533dc6d59bddb04d2281e0e
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-06-07 15:29:10 +0200

    add header

---
+++ src/components/Header/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <img src="../../assets/img/spotify-logo.png" alt="logo-spotify">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "BaseHeader"
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        background-color: var(--primary-color);
+
+        img{
+            width: 80px;
+            padding: 15px;
+        }
+
+    }
+
+</style>
